2527c52f7cb3381c02a0bc8d4156e78d9ab06ece
mhmdrzrasi <mhmdrzrasi@gmail.com>
2024-03-07 18:56:55 +0330

    first commit

---
+++ script.py
@@ -0,0 +1,8 @@
+####################################
+# Author: MohammadReza Rasi
+# Created: 2024-03-07
+# Last Modified: 2024-03-07
+# Description: The Python script monitors various system aspects such as CPU usage, server load, IP blocking, disk I/O consumption, and RAM usage.
+# Usage: python script.py
+####################################
+
